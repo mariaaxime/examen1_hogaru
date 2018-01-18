902694ad6129596c0fccdbd23975dbf7dd9c92ce
Maria Acuna <mariaximenaam@gmail.com>
2018-01-18 00:23:57 +0000

    Agregar home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get '/basics', to: 'static_pages#basics'
+
+  get '/ruby', to: 'static_pages#ruby'
+
+  get '/git', to: 'static_pages#git'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
